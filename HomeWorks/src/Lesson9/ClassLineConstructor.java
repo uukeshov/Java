3eb3eab87260661dd222fac007341fbfcf27df24
uukeshov <uukeshov@gmail.com>
2015-11-07 13:44:05 +0600

    next coomit 1

--- HomeWorks/src/Lesson9/ClassLineConstructor.java
+++ HomeWorks/src/Lesson9/ClassLineConstructor.java
@@ -1,9 +1,38 @@
 package Lesson9;
 
 public class ClassLineConstructor {
+	public static int x = 0, begin, tilt, c;
 
-	public ClassLineConstructor() {
-		// TODO Auto-generated constructor stub
+	private static int getX() {
+		return x;
+	}
+
+	private static void setX(int x) {
+		ClassLineConstructor.x = x;
+	}
+
+	private static String getXString() {
+		return Integer.toString(x);
+	}
+
+	private static double getXdouble() {
+		return (int) x;
+	}
+
+	public static int getSpeed(int x, int c) {
+		if (x != 0) {
+			return c;
+		}
+		return 0;
+
+	}
+
+	public static int getBegin(int x) {
+		if (x != 0) {
+			return c;
+		}
+		return 0;
+
 	}
 
 }
